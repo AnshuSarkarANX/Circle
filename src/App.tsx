68c9ec1bd3c7af49e84d9bbf0950fd2e052a5d55
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-04-09 11:56:37 +0530

    shadcnUI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'; 
-import "./globals.css"
-import { Home } from './_root/pages';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import { Home } from "./_root/pages";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-   <main className="flex h-screen">
-    {/* public path */}
-    <Routes>
-        <Route element= {<AuthLayout/>}>        <Route path = "/sign-in" element = {<SigninForm/>}/>
-        <Route path = "/sign-in" element = {<SignupForm/>}/></Route>
+    <main className="flex h-screen">
+      {/* public path */}
+      <Routes>
+        <Route element={<AuthLayout />}>
+          {" "}
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SignupForm />} />
+        </Route>
 
         {/* Private path */}
-        <Route element = {<RootLayout/>}><Route index element={<Home/>}/></Route>
-        
-    </Routes>
-   </main>
-  )
-}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
