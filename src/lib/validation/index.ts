5f0510784d7c0f9a4b90b44e0900b81c03695f11
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-04-10 00:33:08 +0530

    files to create custom form

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+export  const SignupValidation = z.object({
+    name: z.string(),
+    username: z.string().min(4,{message:"Username must be at least 4 characters"}),
+    email: z.string().email(),
+    password: z.string().min(8,{message:"Password must be at least 8 characters"}),
+  });
