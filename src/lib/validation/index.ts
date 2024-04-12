d76e1d7c1736a55f2ee16ce3e9cecab9fb88a66c
Anshu Sarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2024-04-13 00:50:31 +0530

    added SigninValidation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,10 +1,9 @@
-import * as z from "zod"
-
-export const SignupValidation = z.object({
-    name: z.string().min(2, { message: 'Too short' }),
-    username: z.string().min(2, { message: 'Too short' }),
+import { z } from "zod";
+export  const SignupValidation = z.object({
+    name: z.string(),
+    username: z.string().min(4,{ message: 'Username must be at least 4 characters'}),
     email: z.string().email(),
-    password: z.string().min(8, {message: 'Too short'}),
+    password: z.string().min(8,{ message: 'Password must be at least 8 characters'}),
   });
 
   export const SigninValidation = z.object({
