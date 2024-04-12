99099b63d31c4dfbc967ac339c29c9ebf094c250
triparna01 <166366017+triparna01@users.noreply.github.com>
2024-04-12 18:33:37 +0530

    Update index.ts

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,7 +1,13 @@
-import { z } from "zod";
-export  const SignupValidation = z.object({
-    name: z.string(),
-    username: z.string().min(4,{ message: 'Username must be at least 4 characters'}),
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: 'Too short' }),
+    username: z.string().min(2, { message: 'Too short' }),
     email: z.string().email(),
-    password: z.string().min(8,{ message: 'Password must be at least 8 characters'}),
+    password: z.string().min(8, {message: 'Too short'}),
   });
+
+  export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Wrong Password'}),
+  })
