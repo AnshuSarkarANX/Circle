41c52012d0c65c3673715b3e21dcf785545c34b6
triparna01 <166366017+triparna01@users.noreply.github.com>
2024-04-19 14:47:08 +0530

    Update PostCard.tsx

--- src/components/ui/Shared/PostCard.tsx
+++ src/components/ui/Shared/PostCard.tsx
@@ -20,10 +20,7 @@ const PostCard = ({ post }: PostCardProps) => {
         <div className="flex items-center gap-3">
           <Link to={`/profile/${post.creator.$id}`}>
             <img
-              src={
-                post.creator?.imageUrl ||
-                "/assets/icons/profile-placeholder.svg"
-              }
+              src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}  //post? or post
               alt="creator"
               className="w-12 lg:h-12 rounded-full"
             />
